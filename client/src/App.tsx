86997038c3fdd0d6948db1aca3643905c035c049
YSRKEN <ysr.ken@gmail.com>
2021-06-09 21:43:44 +0900

    [add] Bootstrapを追加し、反映されることを確かめる

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
-function App() {
+const App: React.VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="my-3">
+        <Col>
+          <h1 className="text-center">my家計簿</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
